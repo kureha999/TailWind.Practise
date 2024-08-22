39bb1a39526df75916b4464856033517403847cf
めっちゃくれは <metchakureha@No1.local>
2024-08-22 21:33:58 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], 
+  theme: {
+    extend: {},
+    backgroundImage: {
+      "main-bg": "url(../src/img/mountain.jpg)",
+    },
+  },
+  plugins: [],
+}
